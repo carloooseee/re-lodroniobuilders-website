b2cef6ac721d9b9d0c0b87457dbc7d2ad5aac087
Carlos <149790192+carloooseee@users.noreply.github.com>
2026-07-05 17:19:06 +0800

    api keys

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBzzURtHsOeWH8iM4Zgi5vZUKzcpXEji4k",
-  authDomain: "re-lodroniobuilders.firebaseapp.com",
-  projectId: "re-lodroniobuilders",
-  storageBucket: "re-lodroniobuilders.firebasestorage.app",
-  messagingSenderId: "607098506969",
-  appId: "1:607098506969:web:674a4651a26b04e8dc78c8",
-  measurementId: "G-9MRZD03S7R"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
